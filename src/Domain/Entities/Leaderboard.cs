960f2cc0d184a638d64b819e08a77027fe62fae0
MRFOURIT\mrfou <mrfour.it@gmail.com>
2026-04-14 08:08:05 +0700

    Add 2 models

---
+++ src/Domain/Entities/Leaderboard.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class Leaderboard
+{
+    public int Id { get; set; }
+    public required string UserId { get; set; }
+    public float Points { get; set; }
+    public int Rank { get; set; }
+}
---
+++ src/Domain/Entities/UserVocabTestResults.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Entities;
+
+public class UserVocabTestResults
+{
+    public int Id { get; set; }
+    public required string UserId { get; set; }
+    public int Score { get; set; }
+    public DateTime TakenAt { get; set; }
+}
