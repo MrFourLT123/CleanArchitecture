f505b7a21c149b89ff735a154ae18a5c20bbdf68
MrFourLT123 <40837992+MrFourLT123@users.noreply.github.com>
2026-05-03 22:43:44 +0700

    *Add Category API

---
+++ src/Application/Categories/Queries/GetCategories.cs
@@ -0,0 +1,20 @@
+using CleanArchitecture.Application.Common.Interfaces;
+using CleanArchitecture.Domain.Entities;
+
+public record GetCategoriesQuery : IRequest<List<Category>>;
+
+public class GetCategoriesQueryHandler : IRequestHandler<GetCategoriesQuery, List<Category>>
+{
+    private readonly IApplicationDbContext _context;
+
+    public GetCategoriesQueryHandler(IApplicationDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Category>> Handle(GetCategoriesQuery request, CancellationToken cancellationToken)
+    {
+        var listCategory = await _context.Categories.ToListAsync(cancellationToken);
+        return listCategory;
+    }
+}
---
+++ src/Web/Endpoints/Categories.cs
@@ -0,0 +1,53 @@
+
+using CleanArchitecture.Application.Categories.Commands.CreateCategory;
+using CleanArchitecture.Application.Categories.Commands.DeleteCategory;
+using CleanArchitecture.Application.Categories.Commands.UpdateCategory;
+using CleanArchitecture.Domain.Entities;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+public class Categories : IEndpointGroup
+{
+    public static void Map(RouteGroupBuilder groupBuilder)
+    {
+        groupBuilder.MapPost(CreateCategory);
+        groupBuilder.MapGet("/categories", GetCategories);
+        groupBuilder.MapPut(UpdateCategory, "{id}");
+        groupBuilder.MapDelete(DeleteCategory, "{id}");
+    }
+
+    [EndpointSummary("Get all Categories")]
+    [EndpointDescription("Retrieves all categories along with their details.")]
+    public static async Task<Ok<List<Category>>> GetCategories(ISender sender)
+    {
+        var query = new GetCategoriesQuery();
+        var categories = await sender.Send(query);
+        return TypedResults.Ok(categories);
+    }
+    [EndpointSummary("Create new Category")]
+    [EndpointDescription("Create new category")]
+    public static async Task<Created<int>> CreateCategory(ISender sender, CreateCategoryCommand command)
+    {
+        var id = await sender.Send(command);
+        return TypedResults.Created($"{nameof(CreateCategory)}/{id}", id);
+    }
+    [EndpointSummary("Update an existing Category")]
+    [EndpointDescription("Updates an existing category using the provided details.")]
+    public static async Task<Results<NoContent, BadRequest>> UpdateCategory(ISender sender, int id, UpdateCategoryCommand command)
+    {
+        if (id != command.Id)
+            return TypedResults.BadRequest();
+
+        await sender.Send(command);
+
+        return TypedResults.NoContent();
+    }
+
+    [EndpointSummary("Delete a Category")]
+    [EndpointDescription("Deletes a category with the specified ID.")]
+    public static async Task<Results<NoContent, NotFound>> DeleteCategory(ISender sender, int id)
+    {
+        await sender.Send(new DeleteCategoryCommand(id));
+
+        return TypedResults.NoContent();
+    }
+}
